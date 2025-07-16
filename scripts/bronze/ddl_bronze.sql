6f1e33d248572ec79dc5d8012c5102dbf6a358f7
Thanh Vu <71974989+thanhvu911@users.noreply.github.com>
2025-07-16 21:11:06 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,10 @@
+/*DDL Script: Create Bronze Tables  
+---  
+Script Purpose:  
+    This script creates tables in the 'bronze' schema, dropping existing tables if they already exist.  
+    Run this script to re-define the DDL structure of 'bronze' tables  
+*/
+
 IF OBJECT_ID('bronze.brands','U') IS NOT NULL
     DROP TABLE bronze.brands;
 -- Brands table
